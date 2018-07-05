9d6c0a785aa72098f9f355c9f27765057718febf
ldyte3 <ldyte3@users.noreply.github.com>
2018-07-06 00:01:39 +0100

    Scoreboard

--- src/main/java/com/cannonmc/auto/ChatMonitor.java
+++ src/main/java/com/cannonmc/auto/ChatMonitor.java
@@ -4,6 +4,8 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.client.settings.KeyBinding;
 import net.minecraft.init.Blocks;
+import net.minecraft.network.play.server.S3BPacketScoreboardObjective;
+import net.minecraft.scoreboard.Scoreboard;
 import net.minecraft.util.BlockPos;
 import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.EnumChatFormatting;
@@ -17,16 +19,38 @@ import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 
+import java.lang.reflect.Array;
+import java.util.Collection;
+
 public class ChatMonitor {
 
     public Minecraft mc;
-
+    public Collection SBNames;
     public String unformattedMessage;
+    public String ScoreboardNames;
+
+
+
+
 
     @SubscribeEvent
     public void onChat(final ClientChatReceivedEvent event) {
-        unformattedMessage = event.message.getUnformattedText();
-        System.out.println("This is a system printed message:" + unformattedMessage);
+        unformattedMessage = event.message.getFormattedText();
+        System.out.println("System msg:" + unformattedMessage);
+
+        SBNames = Minecraft.getMinecraft().theWorld.getScoreboard().getObjectiveNames();
+        System.out.println(SBNames);
+
+        ScoreboardNames = Minecraft.getMinecraft().theWorld.getScoreboard().getObjective().getDisplayName();
+        System.out.println("2018 TEST:" + ScoreboardNames);
+
+
+        //S3BPacketScoreboardObjective scoreobjective = this.mc.theWorld.getScoreboard().func_96539_a(1);
+        //ScoreObjective objective = board.func_96539_a(1);
+
+        if(unformattedMessage.contains("2000")) {
+            System.out.println("You were born in the year 2000!");
+        }
         //mc.thePlayer.addChatMessage(new ChatComponentText(unformattedMessage));
     }
 }
