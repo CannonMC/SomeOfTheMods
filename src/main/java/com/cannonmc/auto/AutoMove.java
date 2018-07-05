6b52167561eef844fabbac2e04372c5cc2d0021c
James Knight <jimdav40@gmail.com>
2018-07-05 19:53:27 +0100

    New movement method and command server fix

--- src/main/java/com/cannonmc/auto/AutoMove.java
+++ src/main/java/com/cannonmc/auto/AutoMove.java
@@ -1,22 +1,21 @@
 package com.cannonmc.auto;
 
-import java.util.concurrent.ThreadLocalRandom;
-
 import org.lwjgl.input.Keyboard;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.client.settings.KeyBinding;
 import net.minecraft.init.Blocks;
+import net.minecraft.server.management.PlayerProfileCache;
 import net.minecraft.util.BlockPos;
 import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.EnumChatFormatting;
 import net.minecraft.util.MathHelper;
+import net.minecraftforge.client.ClientCommandHandler;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 
@@ -42,14 +41,9 @@ public class AutoMove {
 	@Mod.EventHandler
 	public void postinit(final FMLPostInitializationEvent event) {
 		MinecraftForge.EVENT_BUS.register((Object) this);
-		ClientRegistry
-				.registerKeyBinding(this.toggle = new KeyBinding("Toggle automovement", 19, "key.categories.movement"));
-		this.mc = Minecraft.getMinecraft();
-	}
+		ClientRegistry.registerKeyBinding(this.toggle = new KeyBinding("Toggle automovement", 19, "key.categories.movement"));this.mc = Minecraft.getMinecraft();
 
-	@Mod.EventHandler
-	public static void init(FMLServerStartingEvent event) {
-		event.registerServerCommand(new Command());
+		ClientCommandHandler.instance.registerCommand(new Command());
 	}
 
 	@SubscribeEvent
@@ -61,8 +55,6 @@ public class AutoMove {
 		final int keyForward = this.mc.gameSettings.keyBindForward.getKeyCode();
 		final int keyBackwards = this.mc.gameSettings.keyBindBack.getKeyCode();
 		final int keyAttack = this.mc.gameSettings.keyBindAttack.getKeyCode();
-		final int keyLeft = this.mc.gameSettings.keyBindLeft.getKeyCode();
-		final int keyRight = this.mc.gameSettings.keyBindRight.getKeyCode();
 
 		if (this.toggle.isPressed()) {
 			AutoMove.active = !AutoMove.active;
@@ -71,20 +63,13 @@ public class AutoMove {
 				KeyBinding.setKeyBindState(keySprint, Keyboard.isKeyDown(keySprint));
 				KeyBinding.setKeyBindState(keyJump, Keyboard.isKeyDown(keyJump));
 				KeyBinding.setKeyBindState(keyForward, false);
-				KeyBinding.setKeyBindState(keyLeft, false);
-				KeyBinding.setKeyBindState(keyRight, false);
 				KeyBinding.setKeyBindState(keyBackwards, false);
 				KeyBinding.setKeyBindState(keyAttack, false);
-
 			}
 		}
 		if (AutoMove.active) {
 			final KeyBinding toggle2 = this.toggle;
-			int playerRotation = MathHelper.floor_double((double) (mc.thePlayer.rotationYaw * 4.0F / 360.0F) + 0.5D)
-					& 3;
-
-			mc.thePlayer
-					.addChatMessage(new ChatComponentText(EnumChatFormatting.GOLD + Integer.toString(playerRotation)));
+			int playerRotation = MathHelper.floor_double((double) (mc.thePlayer.rotationYaw * 4.0F / 360.0F) + 0.5D) & 3;
 			double currentX = round(mc.thePlayer.posX, 0);
 			double currentZ = round(mc.thePlayer.posZ, 0);
 			int posAcu = 1;
@@ -95,11 +80,72 @@ public class AutoMove {
 			// -X = 1 WEST
 			// -Z = 2 NORTH
 			// +X = 3 EAST
-
-			// System.out.println(posStart);
+			
+			//Prints out player rotation to chat
+			mc.thePlayer.addChatMessage(new ChatComponentText(EnumChatFormatting.GOLD + Integer.toString(playerRotation)));
 			
 			
+			//For moving to a set point
+			if (playerRotation == 0) {
+				if (currentZ < finishZ) {
+					KeyBinding.setKeyBindState(keyForward, true);
+				} else if (currentZ > finishZ){
+					KeyBinding.setKeyBindState(keyBackwards, true);
+				} else {
+					KeyBinding.setKeyBindState(keyForward, false);
+					KeyBinding.setKeyBindState(keyBackwards, false);
+					if (currentX < finishX) {
+						mc.thePlayer.setAngles(-50, 0);	
+					}else if (currentX > finishX) {
+						mc.thePlayer.setAngles(600, 0);	
+					}
+				}
+			}else if(playerRotation == 3) {
+				if (currentX < finishX) {
+					KeyBinding.setKeyBindState(keyForward, true);
+				} else if (currentX > finishX){
+					KeyBinding.setKeyBindState(keyBackwards, true);
+				} else {
+					KeyBinding.setKeyBindState(keyForward, false);
+					KeyBinding.setKeyBindState(keyBackwards, false);
+					if (currentZ > finishZ) {
+						mc.thePlayer.setAngles(-600, 0);			
+					}else if (currentZ < finishZ) {
+						mc.thePlayer.setAngles(600, 0);	
+					}
+				}
+			}else if(playerRotation == 1) {
+				if (currentX > finishX) {
+					KeyBinding.setKeyBindState(keyForward, true);
+				} else if (currentX < finishX){
+					KeyBinding.setKeyBindState(keyBackwards, true);
+				} else {
+					KeyBinding.setKeyBindState(keyForward, false);
+					KeyBinding.setKeyBindState(keyBackwards, false);
+					if (currentZ < finishZ) {
+						mc.thePlayer.setAngles(-600, 0);			
+					}else if (currentZ > finishZ) {
+						mc.thePlayer.setAngles(600, 0);	
+					}
+				}
+			}else if(playerRotation == 2) {
+				if (currentZ > finishZ) {
+					KeyBinding.setKeyBindState(keyForward, true);
+				} else if (currentZ < finishZ){
+					KeyBinding.setKeyBindState(keyBackwards, true);
+				} else {
+					KeyBinding.setKeyBindState(keyForward, false);
+					KeyBinding.setKeyBindState(keyBackwards, false);
+					if (currentX > finishX) {
+						mc.thePlayer.setAngles(-600, 0);			
+					}else if (currentX < finishX) {
+						mc.thePlayer.setAngles(600, 0);	
+					}
+				}
+			}
+			
 
+			//Jump over air
 			if (cooldown != 0) {
 				cooldown -= 1;
 			}
@@ -143,163 +189,36 @@ public class AutoMove {
 					}
 				}
 			}
-
-			if (currentZ != finishZ || currentX != finishX) {
-				if (playerRotation == 0 || playerRotation == 2) {
-					// Handles the Z axis
-					if (currentZ != finishZ) {
-						if (playerRotation == 0) {
-							if (currentZ < finishZ) {
-								KeyBinding.setKeyBindState(keyForward, true);
-								KeyBinding.setKeyBindState(keyLeft, false);
-								KeyBinding.setKeyBindState(keyBackwards, false);
-								KeyBinding.setKeyBindState(keyRight, false);
-							} else {
-								KeyBinding.setKeyBindState(keyForward, false);
-								KeyBinding.setKeyBindState(keyLeft, false);
-								KeyBinding.setKeyBindState(keyBackwards, true);
-								KeyBinding.setKeyBindState(keyRight, false);
-							}
-
-						} else if (playerRotation == 2) {
-							if (currentZ < finishZ) {
-								KeyBinding.setKeyBindState(keyBackwards, true);
-								KeyBinding.setKeyBindState(keyLeft, false);
-								KeyBinding.setKeyBindState(keyForward, false);
-								KeyBinding.setKeyBindState(keyRight, false);
-							} else {
-								KeyBinding.setKeyBindState(keyBackwards, false);
-								KeyBinding.setKeyBindState(keyLeft, false);
-								KeyBinding.setKeyBindState(keyForward, true);
-								KeyBinding.setKeyBindState(keyRight, false);
-							}
-
-						}
-						// Handles the X axis
-					} else if (currentX != finishX) {
-						if (playerRotation == 0) {
-							if (currentX < finishX) {
-								KeyBinding.setKeyBindState(keyLeft, true);
-								KeyBinding.setKeyBindState(keyForward, false);
-								KeyBinding.setKeyBindState(keyBackwards, false);
-								KeyBinding.setKeyBindState(keyRight, false);
-							} else {
-								KeyBinding.setKeyBindState(keyLeft, false);
-								KeyBinding.setKeyBindState(keyForward, false);
-								KeyBinding.setKeyBindState(keyBackwards, false);
-								KeyBinding.setKeyBindState(keyRight, true);
-							}
-
-						} else if (playerRotation == 2) {
-							if (currentX < finishX) {
-								KeyBinding.setKeyBindState(keyRight, true);
-								KeyBinding.setKeyBindState(keyForward, false);
-								KeyBinding.setKeyBindState(keyLeft, false);
-								KeyBinding.setKeyBindState(keyBackwards, false);
-							} else {
-								KeyBinding.setKeyBindState(keyRight, false);
-								KeyBinding.setKeyBindState(keyForward, false);
-								KeyBinding.setKeyBindState(keyLeft, true);
-								KeyBinding.setKeyBindState(keyBackwards, false);
-							}
-
-						}
-					}
-
+			
+			
+			//Jump over block or break block if in the way 
+			if (round(mc.thePlayer.posX, posAcu) == round(mc.thePlayer.lastTickPosX, posAcu)
+					&& round(mc.thePlayer.posZ, posAcu) == round(mc.thePlayer.lastTickPosZ, posAcu)) {
+				if (jumptries < 30) {
+					KeyBinding.setKeyBindState(keyJump, true);
+					jumptries += 1;
 				} else {
-					if (currentX != finishX) {
-						if (playerRotation == 1) {
-							if (currentX > finishX) {
-								KeyBinding.setKeyBindState(keyForward, true);
-								KeyBinding.setKeyBindState(keyLeft, false);
-								KeyBinding.setKeyBindState(keyBackwards, false);
-								KeyBinding.setKeyBindState(keyRight, false);
-							} else {
-								KeyBinding.setKeyBindState(keyForward, false);
-								KeyBinding.setKeyBindState(keyLeft, false);
-								KeyBinding.setKeyBindState(keyBackwards, true);
-								KeyBinding.setKeyBindState(keyRight, false);
-							}
-
-						} else if (playerRotation == 3) {
-							if (currentX > finishX) {
-								KeyBinding.setKeyBindState(keyBackwards, true);
-								KeyBinding.setKeyBindState(keyLeft, false);
-								KeyBinding.setKeyBindState(keyForward, false);
-								KeyBinding.setKeyBindState(keyRight, false);
-							} else {
-								KeyBinding.setKeyBindState(keyBackwards, false);
-								KeyBinding.setKeyBindState(keyLeft, false);
-								KeyBinding.setKeyBindState(keyForward, true);
-								KeyBinding.setKeyBindState(keyRight, false);
-							}
-
-						}
-					}else if (currentZ != finishZ) {
-						if (playerRotation == 1) {
-							if (currentZ < finishZ) {
-								KeyBinding.setKeyBindState(keyLeft, true);
-								KeyBinding.setKeyBindState(keyForward, false);
-								KeyBinding.setKeyBindState(keyBackwards, false);
-								KeyBinding.setKeyBindState(keyRight, false);
-							} else {
-								KeyBinding.setKeyBindState(keyLeft, false);
-								KeyBinding.setKeyBindState(keyForward, false);
-								KeyBinding.setKeyBindState(keyBackwards, false);
-								KeyBinding.setKeyBindState(keyRight, true);
-							}
-
-						} else if (playerRotation == 3) { ////
-							if (currentZ < finishZ) {
-								KeyBinding.setKeyBindState(keyRight, true);
-								KeyBinding.setKeyBindState(keyForward, false);
-								KeyBinding.setKeyBindState(keyLeft, false);
-								KeyBinding.setKeyBindState(keyBackwards, false);
-							} else {
-								KeyBinding.setKeyBindState(keyRight, false);
-								KeyBinding.setKeyBindState(keyForward, false);
-								KeyBinding.setKeyBindState(keyLeft, true);
-								KeyBinding.setKeyBindState(keyBackwards, false);
-							}
-						}
+					if (jumptries == 31) {
+						KeyBinding.setKeyBindState(keyJump, false);
 					}
+					KeyBinding.setKeyBindState(keyAttack, true);
 
-				}
-
-				if (round(mc.thePlayer.posX, posAcu) == round(mc.thePlayer.lastTickPosX, posAcu)
-						&& round(mc.thePlayer.posZ, posAcu) == round(mc.thePlayer.lastTickPosZ, posAcu)) {
-					if (jumptries < 30) {
-						KeyBinding.setKeyBindState(keyJump, true);
-						jumptries += 1;
-					} else {
-						if (jumptries == 31) {
-							KeyBinding.setKeyBindState(keyJump, false);
-						}
-						KeyBinding.setKeyBindState(keyAttack, true);
-
-						jumptries += 1;
-						if (jumptries == 80) {
-							jumptries = 0;
-						}
+					jumptries += 1;
+					if (jumptries == 80) {
+						jumptries = 0;
 					}
-
-				} else {
-
-					jumptries = 0;
-					// KeyBinding.setKeyBindState(keyJump, false);
-					KeyBinding.setKeyBindState(keyAttack, false);
 				}
 
 			} else {
 
-				KeyBinding.setKeyBindState(keyRight, false);
-				KeyBinding.setKeyBindState(keyForward, false);
-				KeyBinding.setKeyBindState(keyLeft, false);
-				KeyBinding.setKeyBindState(keyBackwards, false);
+				jumptries = 0;
+				KeyBinding.setKeyBindState(keyAttack, false);
 			}
+
 		}
 	}
 
+	//Function to round doubles
 	public static double round(double value, int places) {
 		if (places < 0)
 			throw new IllegalArgumentException();
--- src/main/java/com/cannonmc/auto/Command.java
+++ src/main/java/com/cannonmc/auto/Command.java
@@ -11,32 +11,27 @@ import net.minecraft.util.EnumChatFormatting;
 public class Command extends CommandBase{
 	
 	private EntityPlayerSP thePlayer;
-	private Minecraft mc; 
-	//Test
-	@Override
+	public Minecraft mc; 
+    
+	public boolean canCommandSenderUseCommand(final ICommandSender sender) {
+        return true;
+    }
+	
 	public String getCommandName() {
 		return "automove";
 	}
 
-	@Override
 	public String getCommandUsage(ICommandSender sender) {
 		return "/Automove <X> <Z>";
 	}
 
-	@Override
 	public void processCommand(ICommandSender sender, String[] args) throws CommandException {
 		if(args.length == 2) {
-			
-			mc.thePlayer.setAngles(360, 0);
 			AutoMove.finishX = Integer.parseInt(args[0]);
 			AutoMove.finishZ = Integer.parseInt(args[1]);
 			sender.addChatMessage(new ChatComponentText(EnumChatFormatting.GOLD + "Coordinates set!"));
 		}else {
 			sender.addChatMessage(new ChatComponentText(EnumChatFormatting.GOLD + "You need to give an X and Y coordinate,"));
 		}
-		
-		 
 	}
-		
-
 }
