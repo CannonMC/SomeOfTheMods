a0dc90cee8e2d15ec28954f0ed351cca2a1aa521
James Knight <jimdav40@gmail.com>
2018-07-10 20:29:30 +0100

    Update
    - Changed chat detection
    - Fixed end game detection
    - Picks a team to go for when game starts
    - Replies to people

--- src/main/java/com/cannonmc/auto/ChatMonitor.java
+++ src/main/java/com/cannonmc/auto/ChatMonitor.java
@@ -1,8 +1,12 @@
 package com.cannonmc.auto;
 
 import java.util.Collection;
+import java.util.concurrent.TimeUnit;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.EnumChatFormatting;
 import net.minecraftforge.client.event.ClientChatReceivedEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
@@ -14,22 +18,32 @@ public class ChatMonitor {
     public String ScoreboardNames;
 
     @SubscribeEvent
-    public void onChat(final ClientChatReceivedEvent event) {
+    public void onChat(final ClientChatReceivedEvent event) throws InterruptedException {
         unformattedMessage = event.message.getFormattedText();
+        unformattedMessage = EnumChatFormatting.getTextWithoutFormattingCodes(unformattedMessage);
         System.out.println("System msg:" + unformattedMessage);
         //§r§f§lThe Bridge§r§8 - §r§e§l2v2v2v2§r
         //Red wins = §r§c§lRED WINS!§r
         //Blue Wins = §r§9§lBlue WINS!§r
-        if(unformattedMessage.contains("§r§f§lThe Bridge§r§8 - §r§e§l1v1§r")) {
-            AutoMove.active = true;
-            System.out.println("Auto Move has been activated!");
-            GoalPicker.detectPlayersTeam();
-        } else if(unformattedMessage.contains("§r§c§lRED WINS!§r")) {
-            AutoMove.active = false;
-            System.out.println("Auto Move has now been deactivated because red won!");
-        } else if(unformattedMessage.contains("§r§9§lBlue WINS!§r")) {
-            AutoMove.active = false;
-            System.out.println("Auto Move has now been deactivated because blue won!");
+        if (unformattedMessage.startsWith(" ")) {
+        	if(unformattedMessage.contains("The Bridge - 2v2v2v2")) {
+                AutoMove.active = true;
+                System.out.println("AutoMove has been activated!");
+                GoalPicker.detectPlayersTeam();
+                AutoMove.randomTeamPicker();
+            } else if(unformattedMessage.contains(" WINS!")) {
+                AutoMove.active = false;
+                System.out.println("AutoMove has been deactivated!");
+            }
         }
+        
+        //Detects when people are being mean
+        if(unformattedMessage.contains(" bot")) {
+        	if(unformattedMessage.contains(Minecraft.getMinecraft().thePlayer.getName())) {
+        		TimeUnit.SECONDS.sleep(2);
+        		Minecraft.getMinecraft().thePlayer.sendChatMessage("no im not");
+        	}
+        }
+        
     }
 }
